feccc2eb939ad2c7ff2f07d901d5d275e90cc7c3
ritikakoul123 <76279413+ritikakoul123@users.noreply.github.com>
2021-04-12 17:53:27 +0530

    C Program added

--- 10_Factorial.c
+++ 10_Factorial.c
@@ -1,13 +1,15 @@
-//A C Program to calculate Factorial of a number.                
-#include<stdio.h>  
+//A C Program to calculate Factorial of a number.
+// Ritika Koul 
+#include<stdio.h>  //Preprocessor directive to include standard input output header file
 int main()    
 {    
- int i,fact=1,number;    
+ int i,fact=1,number;    // Variable declratation 
  printf("Enter a number: ");    
   scanf("%d",&number);    //It will scan the number entered by user.
     for(i=1;i<=number;i++){    
-      fact=fact*i;   //Formula 
+      fact=fact*i;   //Logic 
   }    
   printf("Factorial of %d is: %d",number,fact);    //This will give us output.
 return 0;  
 }
+// End of loop 
