1c8f577edbdcdb3694665f2e7a4909a6661784f8
ritikakoul123 <76279413+ritikakoul123@users.noreply.github.com>
2021-04-12 17:44:02 +0530

    C Program added

--- 06_VowelConsonants.c
+++ 06_VowelConsonants.c
@@ -1,8 +1,9 @@
-//A C Program to check whether the alphabet is Vowel or Consonant.               © Ishav Verma 18/March/2021
+//A C Program to check whether the alphabet is Vowel or Consonant.             
+// Ritika Koul
 //The letters A, E, I, O and U are called vowels & the remaining ones are calles consonants.
-#include <stdio.h>
-int main() {
-    char c;
+#include <stdio.h> //Preprocessor directive to include standard input output header file
+int main() {  // Startbm of main body 
+    char c; // Variable declratation
     int lowercase_vowel, uppercase_vowel;
     printf("Enter an alphabet: \n");
     scanf("%c", &c); //It will scan the alphabet entered by user.
@@ -14,3 +15,4 @@ int main() {
         printf("%c is a consonant.", c);
     return 0;
 }
+// End of loop
